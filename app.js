c9b2aa377cbdc767587ce4cbbcfe15f6f9dc7dc6
melicom0701 <melicom0701@gmail.com>
2024-01-14 14:03:54 +0700

    port config

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ require("dotenv").config();
 const express = require("express");
 const appRoutes = require('./src/routes')
 const app = express();
-const port = 8080;
+const port =  process.env.PORT;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/", appRoutes);
 
 
-app.listen(port, function () {
+app.listen( port, function () {
   console.log("Your app running on port " + port);
 });
